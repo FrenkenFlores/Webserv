a73e04a41d9b5617fbcc84c683816c846d07938a
FrenkenFlores <fflores@student.21-school.ru>
2021-06-27 19:52:04 +0300

    update #25: added launch script

---
+++ srcs/cmake-build-debug/CMakeFiles/Webserv.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/fflores/Desktop/Webserv/srcs/assign_server_to_socket.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/assign_server_to_socket.cpp.o"
+  "/Users/fflores/Desktop/Webserv/srcs/callback.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/callback.cpp.o"
+  "/Users/fflores/Desktop/Webserv/srcs/callback_utils.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/callback_utils.cpp.o"
+  "/Users/fflores/Desktop/Webserv/srcs/init_socket_list.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/init_socket_list.cpp.o"
+  "/Users/fflores/Desktop/Webserv/srcs/is_fd_ready.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/is_fd_ready.cpp.o"
+  "/Users/fflores/Desktop/Webserv/srcs/launch_server.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/launch_server.cpp.o"
+  "/Users/fflores/Desktop/Webserv/srcs/main.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/main.cpp.o"
+  "/Users/fflores/Desktop/Webserv/srcs/parse_buffer.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/parse_buffer.cpp.o"
+  "/Users/fflores/Desktop/Webserv/srcs/parse_header_functions.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/parse_header_functions.cpp.o"
+  "/Users/fflores/Desktop/Webserv/srcs/parser.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/parser.cpp.o"
+  "/Users/fflores/Desktop/Webserv/srcs/read_headers.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/read_headers.cpp.o"
+  "/Users/fflores/Desktop/Webserv/srcs/remove_client.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/remove_client.cpp.o"
+  "/Users/fflores/Desktop/Webserv/srcs/reset_socket.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/reset_socket.cpp.o"
+  "/Users/fflores/Desktop/Webserv/srcs/similar_get_req.cpp" "/Users/fflores/Desktop/Webserv/srcs/cmake-build-debug/CMakeFiles/Webserv.dir/similar_get_req.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd ./srcs/cmake-build-debug/ && ./Webserv
